69a6f826fcb8df1736597e7d7761410e55611620
Pallavi Gupta <92811572+pallavi030@users.noreply.github.com>
2023-09-28 20:49:46 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,12 @@
+
+
+
+
+const useralert = () => {
+   //console.log(document.querySelector(".card-body").children)
+    const h5 = document.querySelector(".card-body").children[0]
+    
+}
+const bgcolorcard1 = document.querySelector(".card1").addEventListener('click',()=>{
+   setTimeout(useralert,1000);
+})
